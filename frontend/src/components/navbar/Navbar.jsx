97c74c4c0a72c52928fd9f3fe3745da1b43f2c3e
Pawan <pawanb78@gmail.com>
2024-07-11 23:54:51 +0100

    assets export import tested

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Navbar.css'
+import {assets} from '../../assets/frontend_assets/assets'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-
+      <img src={assets.logo} alt="" />
     </div>
   )
 }
