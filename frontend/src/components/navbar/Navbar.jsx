41b91cf151c976f3adf76b5658adbee39fa72eac
Pawan <pawanb78@gmail.com>
2024-08-09 07:09:50 +0100

    cart page done, made responsive and items can be added and removed

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,27 +1,55 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/frontend_assets/assets";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const Navbar = ({setShowLogin}) => {
-  const [menu, setMenu] = useState('Home');
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("Home");
 
   return (
     <div className="navbar">
-      <img className="logo" src={assets.logo} alt="Logo" />
+      <Link to='/'>
+       <img className="logo" src={assets.logo} alt="Logo" />
+      </Link>
       <ul className="navbar-menu">
-        <Link to='/' onClick={() => setMenu('Home')} className={menu === 'Home' ? 'active' : ''}>Home</Link>
-        <a href="#explore-menu" onClick={() => setMenu('Menu')} className={menu === 'Menu' ? 'active' : ''}>Menu</a>
-        <a href="#app-download" onClick={() => setMenu('Mobile-app')} className={menu === 'Mobile-app' ? 'active' : ''}>Mobile-app</a>
-        <a href="#footer" onClick={() => setMenu('Contact us')} className={menu === 'Contact us' ? 'active' : ''}>Contact us</a>
+        <Link
+          to="/"
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("Mobile-app")}
+          className={menu === "Mobile-app" ? "active" : ""}
+        >
+          Mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("Contact us")}
+          className={menu === "Contact us" ? "active" : ""}
+        >
+          Contact us
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="Search Icon" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="Basket Icon" />
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="Basket Icon" />
+          </Link>
           <div className="dot"></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>Sign in</button>
+        <button onClick={() => setShowLogin(true)}>Sign in</button>
       </div>
     </div>
   );
