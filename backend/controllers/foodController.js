8b63c8add2fb2ac665b16b05049799f81e4802d8
Pawan <pawanb78@gmail.com>
2024-08-10 14:02:11 +0100

    list food and remove food api created and tested with thunder client

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,27 +1,50 @@
 import foodModel from "../models/foodModel.js";
-import fs from 'fs';
+import fs from "fs";
 
 // add food item
 
-const addFood = async (req, res) =>{
+const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
 
-    let image_filename = `${req.file.filename}`;
-    const food = new foodModel({
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        category: req.body.category,
-        image: image_filename,
-    })
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error Saving the Food" });
+  }
+};
 
+// all food list
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ sucess: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error getting images" });
+  }
+};
+
+
+// remove food item
+const removeFood = async(req, res) =>{
     try {
-        await food.save();
-        res.json({success:true, message: 'Food Added Successfully'})
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: 'Food Removed'})
     } catch (error) {
-        console.log(error)
-        res.json({success: false, message: 'Error Saving the Food'})
+        console.log(error);
+        res.json({success: false, message: 'Error deleting the image'})
     }
 
 }
-
-export {addFood};
+export { addFood, listFood, removeFood };
