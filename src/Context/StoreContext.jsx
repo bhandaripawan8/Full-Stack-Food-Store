5b9c4bcd85ed870df14d2faa934373466fd6d6a8
Pawan <pawanb78@gmail.com>
2024-08-10 08:35:14 +0100

    deleted frontend folder directory

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -20,12 +20,21 @@ const StoreContextProvider = (props) =>{
         setCartItem((prev)=>({...prev, [itemId]: prev[itemId]-1}))
     }
 
-    useEffect(()=>{
-        console.log(cartItems)
-    })
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for(const item in cartItems) {
+            {
+                if (cartItems[item]>0){
+                    let itemInfo = food_list.find((product)=>product._id === item);
+                    totalAmount += itemInfo.price*cartItems[item]
+                }
+            }
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
-        food_list, cartItems, setCartItem, addToCart, removeFromCart
+        food_list, cartItems, setCartItem, addToCart, removeFromCart, getTotalCartAmount
     }
     
     return (
